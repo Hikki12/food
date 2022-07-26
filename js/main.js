a7be582c1a5bc5f33c0333ff80cf1b1d7850f856
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-07-26 13:46:53 -0500

    index view updated

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 window.addEventListener('scroll', () => {
     const header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 0)
-})
+});
+
+
+const menuToggle = document.querySelector('.menuToggle');
+const navigation = document.querySelector('.navigation');
+
+const toggleMenu = () => {
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
+const closeMenu = () => {
+    menuToggle.classList.remove('active');
+    navigation.classList.remove('active');
+}
+
+menuToggle.addEventListener('click', toggleMenu);
